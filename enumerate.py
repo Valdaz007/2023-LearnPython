99e9f5915840bf6f6f2a4b1ccf2e378cb0665cd5
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-04-05 20:28:17 +1000

    Create enumerate.py

---
+++ enumerate.py
@@ -0,0 +1,15 @@
+myList = ['Victor', 'Absalom', 'Arnold', 'Zuriel', 'Ezekiel']
+
+def main(alist):
+  for index, item in alist:
+    print(f"{index} {_list}")
+
+if __name__ == "__main__":
+  main()
+  
+# Expected Output:
+# 0 Victor
+# 1 Absalom
+# 2 Arnold
+# 3 Zuriel
+# 4 Exekiel
