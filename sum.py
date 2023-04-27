74f2d01a8bbe1823c62db922dc1e01ca91285fda
Victor Volsavai <59840456+Valdaz007@users.noreply.github.com>
2023-04-28 08:53:35 +1000

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,5 @@
+# Get the Sum of all the Elements of a List
+# Using the sum() function we can add all the elements of the list and return the sum.
+
+def main(alist: List(int)) -> int:
+  return sum(alist)
